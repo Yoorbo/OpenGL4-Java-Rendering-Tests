0dcdc647d90bfe12328a9781c5d227d87076960f
Yann Berton <yannberton.yb@gmail.com>
2022-07-15 08:56:49 +0200

    Added AbstractVector Class as parent

---
+++ src/datastructures/AbstractVector.java
@@ -0,0 +1,31 @@
+package datastructures;
+
+public abstract class AbstractVector {
+	public float x = 1.0f;
+	public float y = 1.0f;
+	public float z = 1.0f;
+	
+	public AbstractVector(float xx, float yy, float zz) {
+		x = xx; y = yy; z = zz;
+	}
+	
+	float[] asArray() {
+		return new float[] {
+				x, y, z
+		};
+	}
+	
+	float[] insertFront(float[] arrayToCombine) {
+		float result[] = new float[3 + arrayToCombine.length];
+		System.arraycopy(asArray(), 0, result, 0, 3);
+		System.arraycopy(arrayToCombine, 0, result, 3, arrayToCombine.length);
+		return result;
+	}
+	
+	float[] insertBehind(float[] arrayToCombine) {
+		float result[] = new float[3 + arrayToCombine.length];
+		System.arraycopy(arrayToCombine, 0, result, 0, arrayToCombine.length);
+		System.arraycopy(asArray(), 0, result, arrayToCombine.length, 3);
+		return result;
+	}
+}
--- src/datastructures/Position.java
+++ src/datastructures/Position.java
@@ -1,31 +1,8 @@
 package datastructures;
 
-public class Position {
-	public float x = 1.0f;
-	public float y = 1.0f;
-	public float z = 1.0f;
-	
+public class Position extends AbstractVector {
+
 	public Position(float xx, float yy, float zz) {
-		x = xx; y = yy; z = zz;
-	}
-	
-	float[] asArray() {
-		return new float[] {
-				x, y, z
-		};
-	}
-	
-	float[] insertFront(float[] arrayToCombine) {
-		float result[] = new float[3 + arrayToCombine.length];
-		System.arraycopy(asArray(), 0, result, 0, 3);
-		System.arraycopy(arrayToCombine, 0, result, 3, arrayToCombine.length);
-		return result;
-	}
-	
-	float[] insertBehind(float[] arrayToCombine) {
-		float result[] = new float[3 + arrayToCombine.length];
-		System.arraycopy(arrayToCombine, 0, result, 0, arrayToCombine.length);
-		System.arraycopy(asArray(), 0, result, arrayToCombine.length, 3);
-		return result;
+		super(xx, yy, zz);
 	}
 }
